50a1e520368f7c6bc19d72296753fbf2d9f4ba8a
Mohammadnaved Naved <36394261+navedmohammad@users.noreply.github.com>
2024-11-26 14:49:53 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,5 +1,5 @@
 api_id = 22475741
 api_hash = "1a217be71a0225e0a678af286c211f8a"
-bot_token = "6313976047:AAGfjTjnkP44Iy5bd1X0yHCL-xM-qIQ8NvY"
-AUTH = 6401497985
+bot_token = "8029869301:AAFvO7-0zSTyPHG7QjCsqcMnIYHamsAbw-o"
+AUTH = 6562642609
 #Bot Username = "hacejjebdbbot"
